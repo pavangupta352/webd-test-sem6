e22e1470046bd9d1ee3c6a2f9e67c7e139f06a13
Pavan Gupta <77353814+pavangupta352@users.noreply.github.com>
2023-03-03 16:30:04 +0530

    Add files via upload

---
+++ model.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+module.exports = mongoose.model("blogger",new mongoose.Schema({
+
+    title:"string",
+    description:"string",
+    markdown:"string",
+    date:Date,
+    id:String
+  
+ }))
---
+++ restful.js
@@ -0,0 +1,111 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const methodOverride = require("method-override");
+
+const { v4: uuid } = require('uuid');
+
+const dotenv = require("dotenv")
+dotenv.config({ path: 'config.env' });
+
+const mongoose=require("mongoose");
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(methodOverride('_method'))
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI).then(()=> console.log("Connected t MongoDB"))
+
+.catch((err)=> console.error('Could not connect to MongoDB', err))
+const blogdata=require("./model")
+
+// Define routes
+
+app.get('/comments',async (req, res) => {
+    const comments=await blogdata.find();
+
+    res.render('index', { comments });
+
+});
+
+app.get('/comments/new', (req, res) => {
+    res.render('new');
+
+});
+app.post('/comments', async (req,res)=>{
+
+
+    
+    const {title, desc,markdown} = req.body;
+    
+    const newcomment=new blogdata(
+    
+        {
+            title:title,
+    
+            description:desc,
+    
+            markdown:markdown,
+    
+            date:new Date(),
+            id:uuid(),
+    
+        }
+    )
+    
+    
+    const ans=await newcomment.save()
+    res.redirect('/comments');
+
+    
+})
+app.get('/comments/:commentid', async (req,res)=>{
+
+    const {commentid} = req.params;
+    const comment=await blogdata.findOne({id:commentid});
+       
+res.render('show', {comment})
+
+})
+app.get('/comments/:commentid/edit',async (req,res)=>{
+
+    const {commentid} = req.params;
+     const comment=await blogdata.findOne({id:commentid});
+    res.render('edit', {comment})
+
+
+})
+app.patch('/comments/:commentid',async (req,res)=>{
+
+
+    const {commentid} = req.params;
+
+    const comment=await blogdata.findOne({id:commentid});
+    comment.title = req.body.title;
+    
+    comment.description = req.body.description;
+    
+    comment.markdown = req.body.markdown;
+    await comment.save()
+
+    res.redirect('/comments')
+
+})
+app.delete('/comments/:commentid', async (req,res)=>{
+
+    const {commentid} = req.params;
+    const ress=await blogdata.findOneAndDelete({id:commentid})
+  
+    res.redirect('/comments')
+
+})
+// Start the server
+app.listen(3000,()=>{
+    console.log('Server started on port 3000');
+  });
